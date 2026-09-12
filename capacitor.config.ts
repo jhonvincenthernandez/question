9673b68b8db40b1ae976f629cf1ad6621c988e72
jhonvincenthernandez <jhonvincenthernandez9@gamil.com>
2026-09-12 19:37:00 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'act-conversation',
+  webDir: 'dist'
+};
+
+export default config;
